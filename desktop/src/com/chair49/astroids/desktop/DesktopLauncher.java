d931b290ada5e17ea5dbc6f9b58f854d70afae3c
Jacob Reckhard <jacobreckhard@gmail.com>
2017-10-29 09:05:04 -0600

    added libgdx

---
+++ desktop/src/com/chair49/astroids/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.chair49.astroids.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chair49.astroids.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
