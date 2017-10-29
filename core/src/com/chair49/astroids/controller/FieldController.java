2949742339dd7b064c0998424c5da7277eed09f2
Kyle Hennig <henn.kyle@outlook.com>
2017-10-29 10:27:33 -0600

    Pass time deltas to the world

--- core/src/com/chair49/astroids/controller/FieldController.java
+++ core/src/com/chair49/astroids/controller/FieldController.java
@@ -6,9 +6,7 @@ import com.chair49.astroids.model.AsteroidField;
  * Created by jacob on 29/10/17.
  */
 public class FieldController {
-
-
-
     public void update(AsteroidField model, float delta) {
+        model.getWorld().step(delta, 6, 2);
     }
 }
