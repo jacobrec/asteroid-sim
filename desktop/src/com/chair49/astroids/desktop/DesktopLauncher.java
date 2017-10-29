83dd0df617b5b53076124f5499dfe905f1c3eade
Jacob Reckhard <jacobreckhard@gmail.com>
2017-10-29 09:14:39 -0600

    screen config

--- desktop/src/com/chair49/astroids/desktop/DesktopLauncher.java
+++ desktop/src/com/chair49/astroids/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.chair49.astroids.MyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 3840/2;
+        config.height = 2160/2;
+		//config.fullscreen = true;
+
 		new LwjglApplication(new MyGame(), config);
 	}
 }
