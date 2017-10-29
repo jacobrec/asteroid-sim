dbd3161fc32c9baf471d754a954390c0d8a98beb
Jacob Reckhard <jacobreckhard@gmail.com>
2017-10-29 10:31:01 -0600

    fixed render and cleaned collidable

--- core/src/com/chair49/astroids/model/Collidable.java
+++ core/src/com/chair49/astroids/model/Collidable.java
@@ -12,37 +12,10 @@ import java.util.List;
  * Created by jacob on 29/10/17.
  */
 public abstract class Collidable {
-    Polygon collision;
-    Rectangle boundingBox;
-
-    Vector2 position;
-    Vector2 acceleration;
-
-    double mass;
-
     Body body;
-
     List<Fixture> segments;
 
-    public Polygon getCollisionPolygon() {
-        return collision;
-    }
 
-    public Rectangle getBoundingBox() {
-        return boundingBox;
-    }
-
-    public Vector2 getPosition() {
-        return position;
-    }
-
-    public Vector2 getAcceleration() {
-        return acceleration;
-    }
-
-    public double getMass() {
-        return mass;
-    }
 
     public Body getBody() {
         return body;
@@ -52,25 +25,7 @@ public abstract class Collidable {
         return segments;
     }
 
-    public void setCollisionPolygon(Polygon collision) {
-        this.collision = collision;
-    }
 
-    public void setBoundingBox(Rectangle boundingBox) {
-        this.boundingBox = boundingBox;
-    }
-
-    public void setPosition(Vector2 position) {
-        this.position = position;
-    }
-
-    public void setAcceleration(Vector2 acceleration) {
-        this.acceleration = acceleration;
-    }
-
-    public void setMass(double mass) {
-        this.mass = mass;
-    }
 
     public void setBody(Body body) {
         this.body = body;
--- core/src/com/chair49/astroids/view/FieldRenderer.java
+++ core/src/com/chair49/astroids/view/FieldRenderer.java
@@ -12,27 +12,13 @@ import com.chair49.astroids.model.AsteroidField;
  * Created by jacob on 29/10/17.
  */
 public class FieldRenderer {
-    ShapeRenderer sr;
     OrthographicCamera cam;
     Box2DDebugRenderer debugRenderer;
-
-    float box_to_world;
-    Matrix4 debugMatrix;
 
     public FieldRenderer() {
         // Creates new camera object, needed for the shape renderer
         cam = new OrthographicCamera();
         cam.setToOrtho(false, 16, 9);
-
-        // Shape renderer is used for drawing polygons and other geometric shapes
-        sr = new ShapeRenderer();
-        // This sets it to use the camera coordinates instead of the native screen coordinates
-        sr.setProjectionMatrix(cam.combined);
-
-        box_to_world = Gdx.graphics.getWidth() / cam.viewportWidth;
-
-        debugMatrix = new Matrix4(cam.combined);
-        debugMatrix.scale(box_to_world, box_to_world, 1f);
 
         debugRenderer = new Box2DDebugRenderer();
 
@@ -40,17 +26,7 @@ public class FieldRenderer {
 
     public void render(AsteroidField model, float delta) {
 
-        debugRenderer.render(model.getWorld(), debugMatrix);
+        debugRenderer.render(model.getWorld(), cam.combined);
 
-// Use this if we want more control over rendering
-//        sr.begin(ShapeRenderer.ShapeType.Line);
-//        for (Asteroid asteroid : model.asteroids) {
-//            drawAsteroid(asteroid);
-//        }
-//        sr.end();
-    }
-
-    private void drawAsteroid(Asteroid asteroid) {
-        sr.polygon(asteroid.getCollisionPolygon().getTransformedVertices());
     }
 }
