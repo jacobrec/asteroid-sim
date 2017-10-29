cb7fe30c0677912df76bea8611f72429b36fd500
Jacob Reckhard <jacobreckhard@gmail.com>
2017-10-29 09:56:01 -0600

    Added wd placholder

--- core/src/com/chair49/astroids/view/FieldRenderer.java
+++ core/src/com/chair49/astroids/view/FieldRenderer.java
@@ -6,6 +6,11 @@ import com.chair49.astroids.model.AsteroidField;
  * Created by jacob on 29/10/17.
  */
 public class FieldRenderer {
+
+    public FieldRenderer(){
+
+    }
+
     public void render(AsteroidField model, float delta) {
     }
 }
